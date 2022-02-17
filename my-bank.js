3c47753cedee5ff2c601e978c8fbce95de81ded2
maruf sheik <samimunna49@gmail.com>
2022-02-17 16:08:32 +0600

    all done

--- my-bank.js
+++ my-bank.js
@@ -2,43 +2,64 @@
 function inputAmont(input) {
     const amontPaid = document.getElementById(input);
     const total = amontPaid.value;
-    if (total < 0 || isNaN(total)) {
-        alert("opps! not allow negative number and string");
-
-    } else {
-        return total;
-    };
+    //amontPaid.value = '';
+    return total;
 };
+
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const foodAmont = inputAmont('paid-food');
     const rentAmont = inputAmont('paid-rent');
     const clothesAmont = inputAmont('paid-clothes');
-    let totalAmont = parseInt(foodAmont) + parseInt(rentAmont) + parseInt(clothesAmont);
-
-
-    //now last amont after paid
     const totalIncome = inputAmont('my-income');
-    let balance = parseInt(totalIncome) - parseInt(totalAmont);
-    const totalExpenses = document.getElementById('total-expenses').innerText = totalAmont;
-    const haveBalance = document.getElementById('have-balance').innerText = balance;
 
+    // Check error for input
+    if (foodAmont < 0 || isNaN(foodAmont)) {
+        alert('opps! not allow negative number and string, Please enter positive number in Food.');
+    } else if (rentAmont < 0 || isNaN(rentAmont)) {
+        alert('opps! not allow negative number and string, Please enter positive number in Rent.');
+    }
+    else if (clothesAmont < 0 || isNaN(clothesAmont)) {
+        alert('opps! not allow negative number and string, Please enter positive number in clothes.');
+    }
+    else if (totalIncome < 0 || isNaN(totalIncome)) {
+        alert('opps! not allow negative number and string, Please enter positive number in Income.');
+
+    } else {
+        let totalAmont = parseInt(foodAmont) + parseInt(rentAmont) + parseInt(clothesAmont);
+
+        //now last amont after paid
+        let balance = parseInt(totalIncome) - parseInt(totalAmont);
+
+        //add error meassage income  totalamount
+        if (totalAmont > totalIncome) {
+            alert('opps! your income is low');
+        } else {
+            const totalExpenses = document.getElementById('total-expenses').innerText = totalAmont;
+            const haveBalance = document.getElementById('have-balance').innerText = balance;
+        };
+    };
 });
+
 
 //----------------------Saving-------------------------//
 document.getElementById('save-btn').addEventListener('click', function () {
-    const myIncome = document.getElementById('my-income');
-    const totalIncome = myIncome.value;
-    const save = document.getElementById('saving');
-    const amontSave = save.value;
-    const balance = document.getElementById('have-balance').innerText;
-    let Saveing = parseInt(totalIncome) / 100;
-    let totalSaveing = Saveing * amontSave;
-    let finalBalance = parseInt(balance) - parseInt(totalSaveing);
+    const totalIncome = inputAmont('my-income');
+    const amontSave = inputAmont('saving');
+    if (amontSave < 0 || isNaN(amontSave)) {
+        alert('opps! not allow negative number and string, Please enter positive number in saveing.');
+    } else {
+        const balance = document.getElementById('have-balance').innerText;
+        let Saveing = parseInt(totalIncome) / 100;
+        let totalSaveing = Saveing * amontSave;
+        let finalBalance = parseInt(balance) - parseInt(totalSaveing);
 
-    //---balance
-    const mySaving = document.getElementById('saving-amount').innerText = totalSaveing;
-    const lastBalance = document.getElementById('remaining-balance').innerText = finalBalance;
-
-
+        //---balance final and error handle
+        if (totalSaveing > balance) {
+            alert('Not enough amounts have!');
+        } else {
+            const mySaving = document.getElementById('saving-amount').innerText = totalSaveing;
+            const lastBalance = document.getElementById('remaining-balance').innerText = finalBalance;
+        };
+    };
 });
