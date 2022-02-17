ae022dca8dc8b4b064a625f40f3e1d4263d1e9c6
maruf sheik <samimunna49@gmail.com>
2022-02-17 12:29:56 +0600

    update

--- my-bank.js
+++ my-bank.js
@@ -1,32 +1,31 @@
-//----totalpaid ----
-function totalPaid(inputPaid) {
-    const amontPaid = document.getElementById(inputPaid);
+//----totalpaid and myIncome value ----
+function inputAmont(input) {
+    const amontPaid = document.getElementById(input);
     const total = amontPaid.value;
-    amontPaid.value = '';
-    return total;
+    if (total < 0 || isNaN(total)) {
+        alert("opps! not allow negative number and string");
+
+    } else {
+        return total;
+    };
 };
 
-
-
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const foodAmont = totalPaid('paid-food');
-    const rentAmont = totalPaid('paid-rent');
-    const clothesAmont = totalPaid('paid-clothes');
+    const foodAmont = inputAmont('paid-food');
+    const rentAmont = inputAmont('paid-rent');
+    const clothesAmont = inputAmont('paid-clothes');
     let totalAmont = parseInt(foodAmont) + parseInt(rentAmont) + parseInt(clothesAmont);
 
-    //--now last amont after paid ---
-    const myIncome = document.getElementById('my-income');
-    const totalIncome = myIncome.value;
-    let balance = parseInt(totalIncome) - parseInt(totalAmont);
 
+    //now last amont after paid
+    const totalIncome = inputAmont('my-income');
+    let balance = parseInt(totalIncome) - parseInt(totalAmont);
     const totalExpenses = document.getElementById('total-expenses').innerText = totalAmont;
     const haveBalance = document.getElementById('have-balance').innerText = balance;
 
 });
 
-
 //----------------------Saving-------------------------//
-
 document.getElementById('save-btn').addEventListener('click', function () {
     const myIncome = document.getElementById('my-income');
     const totalIncome = myIncome.value;
@@ -35,7 +34,6 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const balance = document.getElementById('have-balance').innerText;
     let Saveing = parseInt(totalIncome) / 100;
     let totalSaveing = Saveing * amontSave;
-
     let finalBalance = parseInt(balance) - parseInt(totalSaveing);
 
     //---balance
